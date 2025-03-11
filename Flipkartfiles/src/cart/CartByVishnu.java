190b8043cfb4be58bf992d14d757658c58abb067
vishnuvardhanReddy424 <vardhanreddy23997@gmail.com>
2025-03-11 16:04:24 +0530

    vishnu pull testing for the project cart

--- Flipkartfiles/src/cart/CartByVishnu.java
+++
@@ -1,8 +0,0 @@
-package cart;
-
-public class CartByVishnu {
-	public static void main(String[] args) {
-		System.out.println("this is created by the vishnu for verification");
-	}
-
-}
--- Flipkartfiles/src/cart/CartPullTesting.java
+++
@@ -1,8 +0,0 @@
-package cart;
-
-public class CartPullTesting {
-
-	public static void main(String[] args) {
-		System.out.println("this page is created by the vishnu for pull testing");
-	}
-}
